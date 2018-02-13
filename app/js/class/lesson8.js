a5a5dcb9229319ffb1c00aaf48822079681c4188
liulian0519 <1223342263@qq.com>
2018-02-13 19:23:32 +0800

    lesson8

--- app/js/class/lesson8.js
+++ app/js/class/lesson8.js
@@ -23,3 +23,34 @@
    };
    console.log(es5_method.hello(),es6_method.hello());
 }
+{
+    //属性表达式
+    let a = 'b';
+    let es5={
+        a:'c'
+    };
+    let es6={
+        [a]:'c'
+    }
+    console.log(es5,es6)
+}
+{
+    //新增api
+    console.log('字符串',Object.is('abc','abc'),'abc'==='abc');
+    console.log('数组',Object.is([],[]),[]===[]);
+
+    console.log('copy',Object.assign({a:'a'},{a:'b'}));//浅复制
+
+    let test = {k:123,o:456}
+    for(let [key,value] of Object.entries(test)){
+        console.log([key,value]);
+    }
+}
+{
+    //扩展运算符
+    // let {a,b,...c}={a:'teee',b:'kill',c:'ddd',d:'sss'};
+    // c={
+    //     c:'ddd',
+    //     d:'sss'
+    // }
+}
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -9596,6 +9596,10 @@ module.exports = function (regExp, replace) {
 "use strict";
 
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 {
     var a = 1;
     var b = 2;
@@ -9620,6 +9624,58 @@ module.exports = function (regExp, replace) {
         }
     };
     console.log(es5_method.hello(), es6_method.hello());
+}
+{
+    //属性表达式
+    var _a = 'b';
+    var _es = {
+        a: 'c'
+    };
+    var _es2 = _defineProperty({}, _a, 'c');
+    console.log(_es, _es2);
+}
+{
+    //新增api
+    console.log('字符串', Object.is('abc', 'abc'), 'abc' === 'abc');
+    console.log('数组', Object.is([], []), [] === []);
+
+    console.log('copy', Object.assign({ a: 'a' }, { a: 'b' })); //浅复制
+
+    var test = { k: 123, o: 456 };
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = Object.entries(test)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var _step$value = _slicedToArray(_step.value, 2),
+                key = _step$value[0],
+                value = _step$value[1];
+
+            console.log([key, value]);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+}
+{
+    //扩展运算符
+    // let {a,b,...c}={a:'teee',b:'kill',c:'ddd',d:'sss'};
+    // c={
+    //     c:'ddd',
+    //     d:'sss'
+    // }
 }
 
 /***/ })
